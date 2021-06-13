de5c7bfea228d6fd3192eec1267a8f8a4975a574
Yash Palriwal <yash.palriwal1@olacabs.com>
2021-06-13 15:08:47 +0530

    added google directions api class

--- src/main/java/com/palriwal/yash/aggregator/FlagResponseAggregator.java
+++ src/main/java/com/palriwal/yash/aggregator/FlagResponseAggregator.java
@@ -1,5 +1,6 @@
 package com.palriwal.yash.aggregator;
 
+import com.palriwal.yash.api.GoogleDirectionsApi;
 import com.palriwal.yash.dto.GoogleDirectionsRequest;
 import com.palriwal.yash.dto.GoogleDirectionsResponse;
 import com.palriwal.yash.dto.LatLng;
@@ -11,17 +12,14 @@ import com.palriwal.yash.service.waypoint.StepPolylineWayPointServiceImpl;
 import com.palriwal.yash.service.waypoint.WayPointService;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
 public class FlagResponseAggregator {
     private FlagPointService flagPointService = new QueueStrategyFlagPointServiceImpl();
     private WayPointService wayPointService = new StepPolylineWayPointServiceImpl();
+    private GoogleDirectionsApi directionsApi = new GoogleDirectionsApi();
 
     public List<LatLng> getFlagCoordinateResponse(GoogleDirectionsRequest request){
         if(Objects.isNull(request) || Objects.isNull(request.getOrigin()) || Objects.isNull(request.getDestination()))
@@ -29,11 +27,7 @@ public class FlagResponseAggregator {
             log.error("Bad Request");
             return null;
         }
-        String requestUrl = createDirectionsRequestUrl(request);
-        log.info("Url : {}", requestUrl);
-        Client client = ClientBuilder.newClient();
-        GoogleDirectionsResponse response = client.target(requestUrl).request().get(GoogleDirectionsResponse.class);
-        //   should get from HTTPUtils
+        GoogleDirectionsResponse response = directionsApi.getGoogleDirectionsApiResponse(request);
         Long flagDistance = 1500L; // get from config
 
         if(!Objects.isNull(response) && !Objects.isNull(response.getRoutes()) && !response.getRoutes().isEmpty())
@@ -43,28 +37,5 @@ public class FlagResponseAggregator {
             return flagCoordinates;
         }
         return null;
-    }
-
-    public String createDirectionsRequestUrl(GoogleDirectionsRequest request){
-        Map<String, String> queryParams = new HashMap<>();
-        queryParams.put("key", "AIzaSyAEQvKUVouPDENLkQlCF6AAap1Ze-6zMos");
-        queryParams.put("origin", request.getOrigin().getLatLngAsString());
-        queryParams.put("destination", request.getDestination().getLatLngAsString());
-
-        String baseUrl = "https://maps.googleapis.com";
-        String endPoint = "/maps/api/directions/json";
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(baseUrl).append(endPoint);
-
-        final Boolean[] firstElement = {true};
-        queryParams.forEach((key,val) -> {
-            if(firstElement[0]){
-                stringBuilder.append("?").append(key).append("=").append(val);
-                firstElement[0] = false;
-            }
-            else
-                stringBuilder.append("&").append(key).append("=").append(val);
-        });
-        return stringBuilder.toString();
     }
 }
---
+++ src/main/java/com/palriwal/yash/api/GoogleDirectionsApi.java
@@ -0,0 +1,49 @@
+package com.palriwal.yash.api;
+
+import com.palriwal.yash.dto.GoogleDirectionsRequest;
+import com.palriwal.yash.dto.GoogleDirectionsResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class GoogleDirectionsApi {
+    public GoogleDirectionsResponse getGoogleDirectionsApiResponse(GoogleDirectionsRequest request){
+        String requestUrl = createDirectionsRequestUrl(request);
+        log.info("Url : {}", requestUrl);
+        Client client = ClientBuilder.newClient();
+        try{
+            GoogleDirectionsResponse response = client.target(requestUrl).request().get(GoogleDirectionsResponse.class);
+            return response;
+        }catch(Exception e){
+            log.error("Error while getting response from directions api :: {}", e.getMessage());
+        }
+        return null;
+    }
+
+    private String createDirectionsRequestUrl(GoogleDirectionsRequest request){
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("key", "AIzaSyAEQvKUVouPDENLkQlCF6AAap1Ze-6zMos");      // keep in config
+        queryParams.put("origin", request.getOrigin().getLatLngAsString());
+        queryParams.put("destination", request.getDestination().getLatLngAsString());
+
+        String baseUrl = "https://maps.googleapis.com";     // Put these things in config
+        String endPoint = "/maps/api/directions/json";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(baseUrl).append(endPoint);
+
+        final Boolean[] firstElement = {true};
+        queryParams.forEach((key,val) -> {
+            if(firstElement[0]){
+                stringBuilder.append("?").append(key).append("=").append(val);
+                firstElement[0] = false;
+            }
+            else
+                stringBuilder.append("&").append(key).append("=").append(val);
+        });
+        return stringBuilder.toString();
+    }
+}
